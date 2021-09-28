bbb268a7969e713c0043fe6cfd6740bb1c9c2d28
James Geib <jpgeib2@gmail.com>
2021-09-28 12:53:40 -0400

    deleted unnecessary actions/creators, constructed initial reducer for DekuSticks

--- client/src/actions/sticksCounter/index.js
+++ client/src/actions/sticksCounter/index.js
@@ -1,11 +1,4 @@
-import {
-    ADD_DEKU_STICKS,
-    REMOVE_DEKU_STICKS,
-    ADD_FIRST_STICKS_UPGRADE,
-    REMOVE_FIRST_STICKS_UPGRADE,
-    ADD_SECOND_STICKS_UPGRADE,
-    REMOVE_SECOND_STICKS_UPGRADE
-} from "../types";
+import { ADD_DEKU_STICKS, REMOVE_DEKU_STICKS } from "../types";
 
 export const addDekuSticks = () => {
     return {
@@ -16,29 +9,5 @@ export const addDekuSticks = () => {
 export const removeDekuSticks = () => {
     return {
         type: REMOVE_DEKU_STICKS
-    };
-};
-
-export const addFirstSticksUpgrade = () => {
-    return {
-        type: ADD_FIRST_STICKS_UPGRADE
-    };
-};
-
-export const removeFirstSticksUpgrade = () => {
-    return {
-        type: REMOVE_FIRST_STICKS_UPGRADE
-    };
-};
-
-export const addSecondSticksUpgrade = () => {
-    return {
-        type: ADD_SECOND_STICKS_UPGRADE
-    };
-};
-
-export const removeSecondSticksUpgrade = () => {
-    return {
-        type: REMOVE_SECOND_STICKS_UPGRADE
     };
 };
