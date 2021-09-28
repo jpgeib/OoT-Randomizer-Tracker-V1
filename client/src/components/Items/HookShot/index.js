48714f3927064034ca51202529afcf0c62ef3ae6
James Geib <jpgeib2@gmail.com>
2021-09-28 14:53:35 -0400

    created boolean state value for Hookshot, implemented functionality to check if player has said item or not

--- client/src/components/Items/HookShot/index.js
+++ client/src/components/Items/HookShot/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import { Grid, Image } from "semantic-ui-react";
 import HookShotIcon from "../../../assets/images/Hookshot.png";
 
-const HookShot = () => {
+const HookShot = (props) => {
+
+    const { removeHookshot, gotHookshot, hasHookshot, notFound } = props;
+
     return (
         <Grid.Column width={2}>
-            <Image size="mini" src={HookShotIcon} />
+            <Image
+                onClick={gotHookshot}
+                onContextMenu={removeHookshot} 
+                style={(hasHookshot === false && notFound === true) ? { filter: "grayscale(100%)"} : { filter: "grayscale(0%)" }} 
+                size="mini" 
+                src={HookShotIcon} 
+            />
         </Grid.Column>
     );
 }
--- client/src/containers/ItemTracker/SecondRow/index.js
+++ client/src/containers/ItemTracker/SecondRow/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import BigGoronSword from "../../../components/Items/BigGoronSword";
 import FairySlingshot from "../../../components/Items/FairySlingshot";
@@ -9,19 +9,43 @@ import IceArrows from "../../../components/Items/IceArrows";
 import FaroresWind from "../../../components/Items/FaroresWind";
 import DekuShield from "../../../components/Items/DekuShield";
 
-const SecondRow = () => {
-    return (
-        <Grid.Row>
-            <BigGoronSword />
-            <FairySlingshot />
-            <FairyOcarina />
-            <Bombchus />
-            <HookShot />
-            <IceArrows />
-            <FaroresWind />
-            <DekuShield />
-        </Grid.Row>
-    );
+class SecondRow extends Component {
+
+    state = {
+        hasHookshot: false,
+        notFound: true
+    }
+
+    gotHookshot = (e) => {
+        this.setState({ hasHookshot: true });
+        this.setState({ notFound: false });
+    }
+
+    removeHookshot = (e) => {
+        e.preventDefault();
+        this.setState({ hasHookshot: false });
+        this.setState({ notFound: true });
+    }
+
+    render() {
+        return (
+            <Grid.Row>
+                <BigGoronSword />
+                <FairySlingshot />
+                <FairyOcarina />
+                <Bombchus />
+                <HookShot 
+                    gotHookshot={this.gotHookshot}
+                    removeHookshot={this.removeHookshot} 
+                    hasHookshot={this.state.hasHookshot} 
+                    notFound={this.state.notFound} />
+                <IceArrows />
+                <FaroresWind />
+                <DekuShield />
+            </Grid.Row>
+        );
+    }
+
 }
 
 export default SecondRow;
