beaff8c8efe29a3ff631e81b037a1bb98dac81eb
James Geib <jpgeib2@gmail.com>
2021-10-15 16:03:49 -0400

    added functionality to WaterTemple dungeon items

--- client/src/components/DungeonItems/WaterTemple/WaterMedallion/index.js
+++ client/src/components/DungeonItems/WaterTemple/WaterMedallion/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import { Grid, Image } from "semantic-ui-react";
 import WaterMedallionIcon from "../../../../assets/images/WaterMedallion.png";
 
-const WaterMedallion = () => {
+const WaterMedallion = (props) => {
+
+    const { hasWaterMedallion, addWaterMedallion, removeWaterMedallion } = props;
+
     return (
         <Grid.Column width={2}>
-            <Image size="mini" src={WaterMedallionIcon} />
+            <Image
+                onClick={addWaterMedallion}
+                onContextMenu={removeWaterMedallion}
+                style={(hasWaterMedallion === false) ? { filter: "grayscale(100%)" } : { filter: "grayscale(0%)" }} 
+                size="mini" 
+                src={WaterMedallionIcon} />
         </Grid.Column>
     );
 }
--- client/src/containers/Modals/DungeonItemModals/WaterTempleItems/index.js
+++ client/src/containers/Modals/DungeonItemModals/WaterTempleItems/index.js
@@ -9,16 +9,47 @@ import WaterMedallion from "../../../../components/DungeonItems/WaterTemple/Wate
 class WaterTempleItems extends Component {
 
     state = {
-        open: false
+        open: false,
+        hasDungeonMap: false,
+        hasCompass: false
+    }
+
+    componentDidMount() {
+        const waterTempleItemsData = JSON.parse(window.localStorage.getItem("waterTempleItems"));
+        this.setState(waterTempleItemsData);
+        console.log(waterTempleItemsData);
+    }
+
+    componentDidUpdate() {
+        window.localStorage.setItem("waterTempleItems", JSON.stringify(this.state));
     }
 
     closeConfigShow = () => this.setState({ open: true });
 
     close = () => this.setState({ open: false });
 
+    gotDungeonMap = (e) => {
+        this.setState({ hasDungeonMap: true });
+    }
+
+    removeDungeonMap = (e) => {
+        e.preventDefault();
+        this.setState({ hasDungeonMap: false });
+    }
+
+    gotCompass = (e) => {
+        this.setState({ hasCompass: true });
+    }
+
+    removeCompass = (e) => {
+        e.preventDefault();
+        this.setState({ hasCompass: false });
+    }
+
     render() {
 
         const { open } = this.state;
+        const { state, smallKeys, bossKeys, medallions } = this.props;
 
         return (
             <Modal
@@ -30,11 +61,31 @@ class WaterTempleItems extends Component {
                 <Modal.Content>
                     <Grid>
                         <Grid.Row>
-                            <SmallKey />
-                            <BossKey />
-                            <DungeonMap />
-                            <Compass />
-                            <WaterMedallion />
+                            <SmallKey 
+                                counter={state.waterKeys}
+                                addWaterKey={smallKeys.addWaterKey}
+                                removeWaterKey={smallKeys.removeWaterKey}
+                            />
+                            <BossKey 
+                                hasWaterBossKey={state.hasWaterBossKey}
+                                addWaterBossKey={bossKeys.addWaterBossKey}
+                                removeWaterBossKey={bossKeys.removeWaterBossKey}
+                            />
+                            <DungeonMap 
+                                hasDungeonMap={this.state.hasDungeonMap}
+                                gotDungeonMap={this.gotDungeonMap}
+                                removeDungeonMap={this.removeDungeonMap}
+                            />
+                            <Compass 
+                                hasCompass={this.state.hasCompass}
+                                gotCompass={this.gotCompass}
+                                removeCompass={this.removeCompass}
+                            />
+                            <WaterMedallion 
+                                hasWaterMedallion={state.hasWaterMedallion}
+                                addWaterMedallion={medallions.addWaterMedallion}
+                                removeWaterMedallion={medallions.removeWaterMedallion}
+                            />
                         </Grid.Row>
                     </Grid>
                 </Modal.Content>
