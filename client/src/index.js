e72687de7bc64cee44a0983b48e67f69a772d149
James Geib <jpgeib2@gmail.com>
2022-03-05 15:35:43 -0500

    successfully implemented redux-persist middleware to retain fireArrows data on page reload

--- client/src/index.js
+++ client/src/index.js
@@ -4,22 +4,36 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
 
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: "root",
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = createStore(
-  reducers,
+  persistedReducer,
   composeEnhancers(applyMiddleware(reduxThunk))
-)
+);
+
+const persistor = persistStore(store); 
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById("root")
