9acd3cc8b3b69b38f0d251036be755691d7d0a40
James Geib <jpgeib2@gmail.com>
2022-03-17 13:54:06 -0400

    constructed the rest of thirdRowReducer

--- client/src/reducers/thirdRowReducer.js
+++ client/src/reducers/thirdRowReducer.js
@@ -1,8 +1,23 @@
-import { ADD_HYLIAN_SHIELD, REMOVE_HYLIAN_SHIELD, ADD_MIRROR_SHIELD, REMOVE_MIRROR_SHIELD } from "../actions/types";
+import { 
+    ADD_HYLIAN_SHIELD, REMOVE_HYLIAN_SHIELD, 
+    ADD_MIRROR_SHIELD, REMOVE_MIRROR_SHIELD,
+    ADD_BOOMERANG, REMOVE_BOOMERANG,
+    ADD_LENS_OF_TRUTH, REMOVE_LENS_OF_TRUTH,
+    ADD_MAGIC_BEANS, REMOVE_MAGIC_BEANS,
+    ADD_MEGATON_HAMMER, REMOVE_MEGATON_HAMMER,
+    ADD_LIGHT_ARROWS, REMOVE_LIGHT_ARROWS,
+    ADD_NAYRUS_LOVE, REMOVE_NAYRUS_LOVE
+} from "../actions/types";
 
 let INITIAL_STATE = {
     hylianShield: null,
-    mirrorShield: null
+    mirrorShield: null,
+    boomerang: null,
+    lensOfTruth: null,
+    magicBeans: 0,
+    megatonHammer: null,
+    lightArrows: null,
+    nayrusLove: null
 };
 
 export default function (state = INITIAL_STATE, action) {
@@ -15,6 +30,30 @@ export default function (state = INITIAL_STATE, action) {
             return { ...state, mirrorShield: true };
         case REMOVE_MIRROR_SHIELD:
             return { ...state, mirrorShield: false };
+        case ADD_BOOMERANG:
+            return { ...state, boomerang: true };
+        case REMOVE_BOOMERANG:
+            return { ...state, boomerang: false };
+        case ADD_LENS_OF_TRUTH:
+            return { ...state, lensOfTruth: true };
+        case REMOVE_LENS_OF_TRUTH:
+            return { ...state, lensOfTruth: false };
+        case ADD_MAGIC_BEANS:
+            return { ...state, magicBeans: Math.min(10, state.magicBeans + 1) };
+        case REMOVE_MAGIC_BEANS:
+            return { ...state, magicBeans: Math.max(0, state.magicBeans - 1) };
+        case ADD_MEGATON_HAMMER:
+            return { ...state, megatonHammer: true };
+        case REMOVE_MEGATON_HAMMER:
+            return { ...state, megatonHammer: false };
+        case ADD_LIGHT_ARROWS:
+            return { ...state, lightArrows: true };
+        case REMOVE_LIGHT_ARROWS:
+            return { ...state, lightArrows: false };
+        case ADD_NAYRUS_LOVE:
+            return { ...state, nayrusLove: true };
+        case REMOVE_NAYRUS_LOVE:
+            return { ...state, nayrusLove: false };
         default:
             return state;
     }
