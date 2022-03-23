adf2c7c921025579efa399507cb372f0b3d77e3e
James Geib <jpgeib2@gmail.com>
2022-03-23 15:59:32 -0400

    added logic to wallet item component to change image based on current state

--- client/src/components/Items/Wallet/index.js
+++ client/src/components/Items/Wallet/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Grid, Image, Header } from "semantic-ui-react";
-import WalletIcon from "../../../assets/images/ChildsWallet.png";
+import ChildsWalletIcon from "../../../assets/images/ChildsWallet.png";
+import AdultsWalletIcon from "../../../assets/images/fourth-row/Adults-Wallet.png";
+import GiantsWalletIcon from "../../../assets/images/fourth-row/Giants-Wallet.png";
 
 const Wallet = (props) => {
 
@@ -12,7 +14,7 @@ const Wallet = (props) => {
                 onClick={addWallet}
                 onContextMenu={removeWallet} 
                 size="mini" 
-                src={WalletIcon} 
+                src={(((wallet === 99 || wallet === 999) && ChildsWalletIcon) || (wallet === 200 && AdultsWalletIcon) || (wallet === 500 && GiantsWalletIcon))} 
             />
             <Header style={(wallet >= 500) ? { color: "red" } : { color: "white" } } as="h1"><span>{wallet}</span></Header>
         </Grid.Column>
