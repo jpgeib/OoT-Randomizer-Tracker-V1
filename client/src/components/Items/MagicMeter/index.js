7c8f26299106dca319d5973e94f535977bc41692
James Geib <jpgeib2@gmail.com>
2022-03-29 15:56:42 -0400

    implemented reducer logic for magic meter

--- client/src/components/Items/MagicMeter/index.js
+++ client/src/components/Items/MagicMeter/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Grid, Image } from "semantic-ui-react";
 import MagicMeterIcon from "../../../assets/images/fifth-row/Magic-Jar-Small-3D-Icon.png";
+import DoubleMagicMeterIcon from "../../../assets/images/fifth-row/Magic-Jar-Large-3D-Icon.png";
 
-const MagicMeter = () => {
+const MagicMeter = (props) => {
+
+    const { magic, addMagic, removeMagic, doubleMagic, addDoubleMagic, removeDoubleMagic } = props;
+
     return (
         <Grid.Column width={2}>
-            <Image size="mini" src={MagicMeterIcon} />
+            <Image
+                onClick={(magic === true) ? addDoubleMagic : addMagic}
+                onContextMenu={(magic === true && doubleMagic === true) ? removeDoubleMagic : removeMagic}
+                style={(magic === false || magic === null) ? { filter: "grayscale(100%)"} : { filter: "grayscale(0%)" }} 
+                size="mini" 
+                src={(magic === true && doubleMagic === true) ? DoubleMagicMeterIcon : MagicMeterIcon} 
+            />
         </Grid.Column>
     );
 }
